6a3dd8c4491d97f09e82580aacb8d2388a19b58f
shammysaini <40337957+shammysaini@users.noreply.github.com>
2024-05-30 00:25:16 +0530

    Update CouchbaseEricssonApplication.java
    add sop statement

--- src/main/java/couch/CouchbaseEricssonApplication.java
+++ src/main/java/couch/CouchbaseEricssonApplication.java
@@ -8,6 +8,7 @@ public class CouchbaseEricssonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CouchbaseEricssonApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
